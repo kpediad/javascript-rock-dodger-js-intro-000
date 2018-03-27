25f5d1fc71a964a8e39e2da3742319553f3df286
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-27 19:43:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
   for (let i = 0; i < ROCKS.length; i++) {
-    GAME.removeChild(ROCKS[i]);
+    GAME.remove(ROCKS[i]);
   };
   window.removeEventListener('keydown', moveDodger);
   alert ("YOU LOSE!");
